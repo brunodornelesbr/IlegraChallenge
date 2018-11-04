354c30af27c59d2345b682ae20f5b6fbba78ec98
Bruno Dorneles <>
2018-11-04 18:49:12 -0200

    Inital project commit

---
+++ Code/IlegraChallenge/IlegraChallenge/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  IlegraChallenge
+//
+//  Created by Bruno Dorneles on 04/11/18.
+//  Copyright © 2018 Bruno. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
